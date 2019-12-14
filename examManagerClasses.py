ae9e7c2de518eec3c19f345be3535d86fd286265
RonsonGallery <57090631+RonsonGallery@users.noreply.github.com>
2019-12-14 17:18:27 +0200

    updated classes and add functions

--- examManagerClasses.py
+++ examManagerClasses.py
@@ -58,9 +58,51 @@ class Lecturer(Coordinator):
         super()._init_(first_name,last_name,Id,password)
 
 
-class Student:
+class Student(Lecturer):
     def __init__(self,first_name='',last_name='',Id='',password=''):
-        self.first_name=first_name
-        self.last_name=last_name
-        self.Id=Id
-        self.password=password
+          super()._init_(first_name,last_name,Id,password)
+
+class UserData:
+    def __init__(self):
+        self.lecturer_list = []
+
+    def add_Coordinator(self, first_name='',last_name='',Id='',password=''):
+        print("First Name of Coordinator: ")
+        first_name=input()
+        print("Last Name of Coordinator: ")
+        last_name=input()
+        print("Id of Coordinator: ")
+        Id=input()
+        print("password of Coordinator: ")
+        password=input()
+        f=open("Coordinator.txt","a")
+        L=["\n",first_name," ",last_name," ",Id," ",password]
+        f.writelines(L)  
+    def add_Lecturer(self, first_name='',last_name='',Id='',password=''):
+         print("First Name of Lecturer: ")
+         first_name=input()
+         print("Last Name of Lecturer: ")
+         last_name=input()
+         print("Id of Lecturer: ")
+         Id=input()
+         print("password of Lecturer: ")
+         password=input()
+        #self.Lecturer_list.append((first_name,last_name,Id,password))
+         f=open("Lecturerer.txt","a")
+         L=["\n",first_name," ",last_name," ",Id," ",password]
+         f.writelines(L)  
+         
+
+    def add_Student(self, first_name='',last_name='',Id='',password=''):
+        print("First Name of Student: ")
+        first_name=input()
+        print("Last Name of Student: ")
+        last_name=input()
+        print("Id of Student: ")
+        Id=input()
+        print("password of Student: ")
+        password=input()
+       #self.Student_list.append((first_name,last_name,Id,password))
+        f=open("Student.txt","a")
+        L=["\n",first_name," ",last_name," ",Id," ",password]
+        f.writelines(L)  
